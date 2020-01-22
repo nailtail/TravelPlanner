cd55a772c97746a22925b13feb996b5345f6acbf
bank <thanakorn.amnajsatit@gmail.com>
2020-01-22 15:59:56 +0700

    Initial Commit

---
+++ TravelPlannerTests/TravelPlannerTests.swift
@@ -0,0 +1,34 @@
+//
+//  TravelPlannerTests.swift
+//  TravelPlannerTests
+//
+//  Created by Thanakorn on 22/1/2563 BE.
+//  Copyright © 2563 GAS. All rights reserved.
+//
+
+import XCTest
+@testable import TravelPlanner
+
+class TravelPlannerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
